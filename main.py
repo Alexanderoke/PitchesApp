7e6ed680c92dec8889615ee4be7a504fc33f5260
alexanderoke <alexander.oke@student.moringaschool.com>
2022-02-14 15:05:47 +0300

    hard code dummy pitches

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
+
+
+pitches = [
+    {
+        'author': 'Alexander oke',
+        'title': 'pitch 1',
+        'content': 'First post content',
+        'date_posted': 'Febuary 10, 2022'
+    },
+    {
+        'author': 'Alex William',
+        'title': 'pitch 2',
+        'content': 'Second post content',
+        'date_posted': 'Febuary 10, 2022'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html',pitches=pitches)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
