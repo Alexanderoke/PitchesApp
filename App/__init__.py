ab4536f7f17adb7c1ad1464d8221101ab56e81fe
alexanderoke <alexander.oke@student.moringaschool.com>
2022-02-15 11:32:44 +0300

    create registration and loginauthentication

--- App/__init__.py
+++ App/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3c24c3b14050a4fd9f96396d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt=Bcrypt(app)
 
 from App import routes
