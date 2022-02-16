3e8b3b7f025de5d5fb26169fdee73c0acbc6921d
alexanderoke <alexander.oke@student.moringaschool.com>
2022-02-16 06:08:39 +0300

    create an account class form and install pillow

--- App/__init__.py
+++ App/__init__.py
@@ -9,4 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'Login'
+login_manager.login_message_category = 'info'
+
 from App import routes
